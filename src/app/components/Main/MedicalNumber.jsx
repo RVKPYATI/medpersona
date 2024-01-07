08843ed331eae0be4ef9f4f475b124ee200f1810
Vasiliy Rusanov <rvk1986@mail.ru>
2024-01-07 11:04:59 +0300

    editing

--- src/app/components/Main/MedicalNumber.jsx
+++ src/app/components/Main/MedicalNumber.jsx
@@ -1,6 +1,6 @@
 export const MedicalNumber = ({ number, text }) => {
   return (
-    <div className="max-w-[270px] h-[75px] rounded border-[#E2E7EE] bg-[#FFFFFF] flex p-[18px] mb-[10px]">
+    <div className="max-w-[270px] min-h-[75px] rounded border-[#E2E7EE] bg-[#FFFFFF] flex p-[18px] mb-[10px]">
       <div className="bg-blue min-w-[38px] h-9 rounded-full text-[#FFFFFF] font-semibold flex justify-center items-center mr-[18px] ">
         {number}
       </div>
--- src/app/components/Main/Review.jsx
+++ src/app/components/Main/Review.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export const Review = ({ image, date }) => {
   return (
-    <div className="px-5 py-6 bg-[#FFFFFF] border-[#DFE6EF] rounded-lg h-[450px]">
+    <div className="px-5 py-6 bg-[#FFFFFF] border-[#DFE6EF] rounded-lg h-[500px]">
       <Image src={image} width={119} height={114} alt="review1" />
       <p className="text-[19px] font-semibold text-[#000000] mb-2 mt-3">
         Название организации здесь пример текста
