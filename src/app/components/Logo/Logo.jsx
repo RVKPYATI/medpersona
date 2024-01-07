32b3102687057d4f2875c45d7b28f02e067589ce
Vasiliy Rusanov <rvk1986@mail.ru>
2024-01-07 13:54:12 +0300

    logo

--- src/app/components/Logo/Logo.jsx
+++ src/app/components/Logo/Logo.jsx
@@ -1,7 +1,3 @@
 export const Logo = () => {
-  return (
-    <div className="text-[#B4B4B4] text-[40px] font-bold ">
-      telegram rvk1986
-    </div>
-  );
+  return <div className="text-[#B4B4B4] text-[40px] font-bold ">Логотип</div>;
 };
