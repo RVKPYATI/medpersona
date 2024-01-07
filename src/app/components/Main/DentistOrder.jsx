376a5379246f58c3827a92d5f3ad163a8925d337
Vasiliy Rusanov <rvk1986@mail.ru>
2024-01-07 11:07:51 +0300

    editing

--- src/app/components/Main/DentistOrder.jsx
+++ src/app/components/Main/DentistOrder.jsx
@@ -1,7 +1,7 @@
 export const DentistOrder = ({ name }) => {
   return (
     <div>
-      <div className="py-5 px-5 bg-[#F4F7FB] rounded max-w-56 h-[91px] mb-4 relative before:block before:absolute before:bg-blue before:w-1 before:h-full before:top-0 before:left-0">
+      <div className="py-5 px-5 bg-[#F4F7FB] rounded max-w-56 min-h-[91px] mb-4 relative before:block before:absolute before:bg-blue before:w-1 before:h-full before:top-0 before:left-0">
         <p className="text-base font-normal">{name}</p>
         <p className="text-[15px] font-bold text-blue underline">
           Подать заявку
