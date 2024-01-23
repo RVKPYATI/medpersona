0107f59a6b03c80e7be9ccc842745c578dbd7da0
Vasiliy Rusanov <rvk1986@mail.ru>
2024-01-23 09:03:11 +0300

    add telegraf

--- src/app/utils/telegramSend.js
+++ src/app/utils/telegramSend.js
@@ -21,24 +21,3 @@ export async function sendMessageTelegram(path, name, phone, doctor) {
     await bot.telegram.sendDocument(CHAT_ID, { source: path });
   }
 }
-
-// import TelegramBot from "node-telegram-bot-api";
-
-// import { CHAT_ID } from "./constans";
-
-// export async function sendMessageTelegram(path, name, phone, doctor) {
-//   const bot = new TelegramBot(process.env.TELEGRAM_TOKEN);
-
-//   const results = `<b>Заявка с сайта med-persona.ru</b>
-//   <u>Дата: ${new Date().toLocaleDateString("ru")}</u>
-//   Имя: <b>${name}</b>
-//   Номер телефона: <b>${phone}</b>
-//   Специалист: <b>${
-//     doctor === "undefined" || "null" ? "Не определен" : doctor
-//   }</b>`;
-
-//   bot.sendMessage(CHAT_ID, results, {
-//     parse_mode: "HTML",
-//   });
-//   if (path) bot.sendDocument(CHAT_ID, path);
-// }
