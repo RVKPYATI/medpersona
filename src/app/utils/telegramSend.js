b13ad11ab8008fd257cfde9841fda88780f98dc7
Vasiliy Rusanov <rvk1986@mail.ru>
2024-01-23 09:00:45 +0300

    add telegraf

--- src/app/utils/telegramSend.js
+++ src/app/utils/telegramSend.js
@@ -1,20 +1,44 @@
-import TelegramBot from "node-telegram-bot-api";
-
+import { Telegraf } from "telegraf";
 import { CHAT_ID } from "./constans";
 
 export async function sendMessageTelegram(path, name, phone, doctor) {
-  const bot = new TelegramBot(process.env.TELEGRAM_TOKEN);
+  const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
 
+  // Формируем сообщение
   const results = `<b>Заявка с сайта med-persona.ru</b>
-  <u>Дата: ${new Date().toLocaleDateString("ru")}</u>
-  Имя: <b>${name}</b>
-  Номер телефона: <b>${phone}</b>
-  Специалист: <b>${
-    doctor === "undefined" || "null" ? "Не определен" : doctor
+<u>Дата: ${new Date().toLocaleDateString("ru")}</u>
+Имя: <b>${name}</b>
+Номер телефона: <b>${phone}</b>
+Специалист: <b>${
+    doctor === "undefined" || doctor === "null" ? "Не определен" : doctor
   }</b>`;
 
-  bot.sendMessage(CHAT_ID, results, {
-    parse_mode: "HTML",
-  });
-  if (path) bot.sendDocument(CHAT_ID, path);
+  // Отправляем сообщение
+  await bot.telegram.sendMessage(CHAT_ID, results, { parse_mode: "HTML" });
+
+  // Отправляем документ, если путь предоставлен
+  if (path) {
+    await bot.telegram.sendDocument(CHAT_ID, { source: path });
+  }
 }
+
+// import TelegramBot from "node-telegram-bot-api";
+
+// import { CHAT_ID } from "./constans";
+
+// export async function sendMessageTelegram(path, name, phone, doctor) {
+//   const bot = new TelegramBot(process.env.TELEGRAM_TOKEN);
+
+//   const results = `<b>Заявка с сайта med-persona.ru</b>
+//   <u>Дата: ${new Date().toLocaleDateString("ru")}</u>
+//   Имя: <b>${name}</b>
+//   Номер телефона: <b>${phone}</b>
+//   Специалист: <b>${
+//     doctor === "undefined" || "null" ? "Не определен" : doctor
+//   }</b>`;
+
+//   bot.sendMessage(CHAT_ID, results, {
+//     parse_mode: "HTML",
+//   });
+//   if (path) bot.sendDocument(CHAT_ID, path);
+// }
